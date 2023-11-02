439c4573ba7c5b4b60a0acf31126b7953541a8fe
Stanislav Cher <StanislavCher@gmail.com>
2023-11-02 21:27:39 +0300

    edit rrd5 with example from the task solution

--- src/App.js
+++ src/App.js
@@ -1,101 +1,88 @@
-import {Link, Redirect, Route, Switch, useParams} from "react-router-dom";
+import {NavLink, Redirect, Route, Switch, useParams, useRouteMatch} from "react-router-dom";
 
 function App() {
   return (
     <div>
         <h1>App Layout</h1>
-        <Link to='/users'>Users list Page</Link>
+        <NavLink to='/users'>Users list Page</NavLink>
         <Switch>
-            <Route path='/' exact component={AppPage} />
-            <Route path='/users/:userId?/edit' component={UserEditPage} />
-            {/*<Route path='/users/:userId?/profile' component={UserProfilePage} />*/}
-            <Route path='/users/:userId?' component={UsersList} />
-            {/*<Route path='/users/' component={UserPage} />*/}
-            {/*<Route path='/' component={MainPage} />*/}
+            <Route path='/users' component={UsersLayout} />
+            <Route path='/' component={MainPage} />
             <Redirect to='/' />
         </Switch>
     </div>
   )
 }
 
-const AppPage = () => {
+const UsersLayout = () => {
+    const { path } = useRouteMatch()
     return (
         <>
-            {/*<Link to='/users'>Users list Page</Link>*/}
+            <h1>Users Layout</h1>
+            <NavLink to='/'>Main Page</NavLink>
+            <Switch>
+                <Route path={path + '/:userId?/edit'} component={UserEditPage} />
+                <Route path={path + '/:userId?/profile'} component={UserProfilePage} />
+                <Route path={path} exact component={UsersListPage} />
+                <Redirect from={path + '/:userId?'} to={path + '/:userId?/profile'} />
+            </Switch>
+        </>
+    )
+}
+
+const MainPage = () => {
+    return (
+        <>
             <h1>Main Page</h1>
         </>
     )
 }
 
-const UsersList = () => {
-    const params = useParams()
-    const { userId } = params
-    console.log(params)
-
-
+const UsersListPage = () => {
+    const { path } = useRouteMatch()
     return (
         <>
-            <h1>Users Layout</h1>
-            <Link to='/'>Main Page</Link>
-            {!userId ?
-                <div>
-                    <h1>User List Page</h1>
-                    <ul>
-                        <li><Link to='/users/0'>User 0</Link></li>
-                        <li><Link to='/users/1'>User 1</Link></li>
-                        <li><Link to='/users/2'>User 2</Link></li>
-                        <li><Link to='/users/3'>User 3</Link></li>
-                        <li><Link to='/users/4'>User 4</Link></li>
-                    </ul>
-                </div>
-                :
-                <div>
-                    <h1>User Page</h1>
-                    <ul>
-                        <li><Link to='/users'>Users list Page</Link></li>
-                        <li><Link to={`/users/${userId}/edit`}>Edit this User</Link></li>
-                        <span>UserId:{userId}</span>
-                    </ul>
-                </div>
-            }
+            <div>
+                <h1>User List Page</h1>
+                <ul>
+                    {new Array(5).fill('').map((_, index) => {
+                        return <li key={'user_key_' + index}>
+                                   <NavLink to={`${path}/${index}`}>User {index}</NavLink>
+                               </li>
+                    })
+                    }
+                </ul>
+            </div>
         </>
+    )
+}
+
+const UserProfilePage = () => {
+    const { userId } = useParams()
+    return (
+        <div>
+            <h1>User Page</h1>
+            <ul>
+                <li><NavLink to='/users'>Users list Page</NavLink></li>
+                <li><NavLink to={`/users/${userId}/edit`}>Edit this User</NavLink></li>
+                <p>UserId:{userId}</p>
+            </ul>
+        </div>
     )
 }
 
 const UserEditPage = () => {
-    const params = useParams()
-    const { userId } = params
-    console.log(params)
-
+    const { userId } = useParams()
     return (
         <>
-            <h1>Users Layout</h1>
-            <Link to='/'>Main Page</Link>
             <h1>Edit User Page</h1>
             <ul>
-                <li><Link to={`/users/${userId}`}>User profile Page</Link></li>
-                <li><Link to={`/users/${Number(userId) + 1}`}>Another User</Link></li>
-                <li><Link to='/users'>Users list Page</Link></li>
+                <li><NavLink to={`/users/${userId}`}>User profile Page</NavLink></li>
+                <li><NavLink to={`/users/${(+userId + 1) % 5}`}>Another User</NavLink></li>
+                <li><NavLink to='/users'>Users list Page</NavLink></li>
             </ul>
         </>
     )
 }
-
-// const UserProfilePage = () => {
-//     const params = useParams()
-//     const { userId } = params
-//     console.log(params)
-//
-//     return (
-//         <>
-//             <h1>User Profile Page</h1>
-//             <ul>
-//                 <li><Link to={`/users/${userId}/edit`}>User profile Page</Link></li>
-//                 <li><Link to='/users/edit'>Another User</Link></li>
-//                 <li><Link to='/users'>Users list Page</Link></li>
-//             </ul>
-//         </>
-//     )
-// }
 
 export default App;
